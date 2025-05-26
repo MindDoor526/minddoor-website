42f9e6c20c65f445aa86c5528385d04abc86e83d
MindDoor526 <minddoor526@gmail.com>
2025-05-26 15:55:34 +0530

    Fixed the "Join the Waitlist" button

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -6,7 +6,9 @@ const HeroSection = () => {
             <div className="flex flex-col justify-center items-center">
                 <p className="text-8xl text-center font-semibold text-wrap mt-20 text-blue-700">Find the Calm Within Yourself.</p>
                 <p className="text-6xl text-center text-gray-700 mr-10 ml-10 mt-20">MindDoor helps you navigate your inner world and cultivate a deeper sense of peace and purpose.</p>
+                <a href="https://docs.google.com/forms/d/e/1FAIpQLSewbRVfImvrAT773g_V7oN3W0fO7cEzUKaLYzULrXnk2AIQRw/viewform?usp=sharing&ouid=105569821472078853006" target="_blank" rel="noopener noreferrer">                
                 <button className="mt-20 mb-20 text-center text-4xl rounded-full bg-cyan-600 h-35 w-80 cursor-pointer hover:bg-cyan-800 transition duration-400" href="https://docs.google.com/forms/d/e/1FAIpQLSewbRVfImvrAT773g_V7oN3W0fO7cEzUKaLYzULrXnk2AIQRw/viewform?usp=sharing&ouid=105569821472078853006">Join the Waitlist</button>
+                </a>
             </div>
         </div>
     )
